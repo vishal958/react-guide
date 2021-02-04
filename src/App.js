a952d6aa53857e97fb61f8543fbd142968242068
Vishal Maurya <Vishal_Maurya@spe.sony.com>
2021-02-04 15:22:05 +0530

    Added radium package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React ,{ Component } from "react";
 import './App.css'
 import Persons from './Persons/Persons'
+import Radium from 'radium'
 
 // const App = props =>{
 //     const [personsState, setPersonsState] = useState({
@@ -88,7 +89,11 @@ class App extends Component{
             font : 'inherit',
             border : '1px solid blue',
             padding : '8px',
-            cursor : 'pointer'
+            cursor : 'pointer',
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color: 'black'
+            }
         }
         let persons = null
         if(this.state.showState){
@@ -108,6 +113,10 @@ class App extends Component{
                 </div>
             )
             style.backgroundColor = 'red'
+            style[':hover'] = {
+                backgroundColor: 'salmon',
+                color: 'black'
+            }
         }
         let classes =[]
         if(this.state.persons.length <= 2){
@@ -127,4 +136,4 @@ class App extends Component{
         );
     }
 }
-export default App
+export default Radium(App)
