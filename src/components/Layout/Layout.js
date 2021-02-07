eba1c5350c21f980e3c08009fd7bf348291462c8
Vishal Maurya <Vishal_Maurya@spe.sony.com>
2021-02-07 15:42:02 +0530

    Navigation Items added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Aux from '../../HigherOrderComponent/Auxilary'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+//import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import './Layout.css'
 
 const Layout = (props) => {
     return (
         <Aux>
             <Toolbar />
+            {/* <SideDrawer /> */}
             <main className="Content">
                 {props.children}
             </main>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Toolbar.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../../Navigation/NavigationItems/NavigationItems'
 
 const Toolbar = (props) => {
     return (
         <header className = "Toolbar">
             <div> MENU </div>
-           <Logo />
+            <Logo />
             <nav>
-                ...
+            <NavigationItems />
             </nav>
         </header>
     )
