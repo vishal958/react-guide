834d29bf406cfdbbcc8f95895a9ddf692461add7
Vishal Maurya <Vishal_Maurya@spe.sony.com>
2021-02-07 12:41:17 +0530

    Added Toolbar navigation and logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Aux from '../../HigherOrderComponent/Auxilary'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import './Layout.css'
 
 const Layout = (props) => {
     return (
         <Aux>
-            <div>
-
-            </div>
-            <main>
+            <Toolbar />
+            <main className="Content">
                 {props.children}
             </main>
         </Aux>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Toolbar.css'
+import Logo from '../../Logo/Logo'
+
+const Toolbar = (props) => {
+    return (
+        <header className = "Toolbar">
+            <div> MENU </div>
+           <Logo />
+            <nav>
+                ...
+            </nav>
+        </header>
+    )
+}
+
+export default Toolbar
